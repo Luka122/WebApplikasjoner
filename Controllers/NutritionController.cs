4fb3660dc30bbcfdab8964bc6349d0c451eed962
Luka <dordeluk1@gmail.com>
2024-11-27 17:51:32 +0100

    redesigned nutrition table, Async database operations for controller

--- Controllers/NutritionController.cs
+++ Controllers/NutritionController.cs
@@ -2,31 +2,28 @@ using Microsoft.AspNetCore.Mvc;
 using Exam.Models;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks; 
 
 public class NutritionController : Controller
 {
-
     private readonly NutritionEntryDbContext _nutritionContext;
 
     public NutritionController(NutritionEntryDbContext nutritionContext)
     {
         _nutritionContext = nutritionContext;
     }
-
-    public IActionResult Index()
+    
+    public async Task<IActionResult> Index()
     {
-
-        List<NutritionEntry> entries = _nutritionContext.Entries.ToList();
-
+        List<NutritionEntry> entries = await _nutritionContext.Entries.ToListAsync();
         return View("NutritionTable", entries);
     }
-
+    
     [HttpPost]
-    public IActionResult Create(NutritionEntry entry)
+    public async Task<IActionResult> Create(NutritionEntry entry)
     {
         if (ModelState.IsValid)
         {
-
             var newEntry = new NutritionEntry
             {
                 Name = entry.Name,
@@ -38,34 +35,35 @@ public class NutritionController : Controller
             };
 
             _nutritionContext.Entries.Add(newEntry);
-
-            _nutritionContext.SaveChanges();
+            await _nutritionContext.SaveChangesAsync(); 
 
             return RedirectToAction("Index");
         }
 
-        // Error Response, Not implemented
+        // Error Response, not implemented
         return Json(new { success = false, message = "Invalid data." });
     }
     
-
     [HttpGet]
-    public IActionResult Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
-        var item = _nutritionContext.Entries.Find(id);
+        var item = await _nutritionContext.Entries.FindAsync(id);
         if (item == null)
         {
             return NotFound();
         }
+
         _nutritionContext.Entries.Remove(item);
-        _nutritionContext.SaveChanges();
+        await _nutritionContext.SaveChangesAsync();
+
         return RedirectToAction("Index");
     }
 
+
     [HttpGet]
-    public IActionResult Update(int id)
+    public async Task<IActionResult> Update(int id)
     {
-        var item = _nutritionContext.Entries.Find(id);
+        var item = await _nutritionContext.Entries.FindAsync(id);
         if (item == null)
         {
             return NotFound();
@@ -73,16 +71,16 @@ public class NutritionController : Controller
         return View(item);
     }
 
+
     [HttpPost]
-    public IActionResult Update(NutritionEntry entry)
+    public async Task<IActionResult> Update(NutritionEntry entry)
     {
         if (ModelState.IsValid)
         {
             _nutritionContext.Entries.Update(entry);
-            _nutritionContext.SaveChanges();
+            await _nutritionContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
         return View(entry);
     }
-    
 }
--- Models/DBInit.cs
+++ Models/DBInit.cs
@@ -6,26 +6,27 @@ public static class DBInit
     public static void Seed(IApplicationBuilder app)
     {
         using var serviceScope = app.ApplicationServices.CreateScope();
-        NutritionEntryDbContext context = serviceScope.ServiceProvider.GetService<NutritionEntryDbContext>();
+        NutritionEntryDbContext? context = serviceScope.ServiceProvider.GetService<NutritionEntryDbContext>();
 
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
-
-        if (!context.Entries.Any())
+        if (context != null)
         {
-            var entries = new List<NutritionEntry>
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            if (!context.Entries.Any())
             {
-                new NutritionEntry { NutritionId = 1, Name = "Apple", FoodGroup = "Fruits", Calories = 95, Protein = 0.5, Fat = 0.3, Carbohydrates = 25 },
-                new NutritionEntry { NutritionId = 2, Name = "Chicken Breast", FoodGroup = "Meats", Calories = 165, Protein = 31, Fat = 3.6, Carbohydrates = 0 },
-                new NutritionEntry { NutritionId = 3, Name = "Broccoli", FoodGroup = "Vegetables", Calories = 55, Protein = 4.0, Fat = 0.6, Carbohydrates = 11 },
-                new NutritionEntry { NutritionId = 4, Name = "Rice", FoodGroup = "Grains", Calories = 206, Protein = 4.3, Fat = 0.4, Carbohydrates = 45 },
-                new NutritionEntry { NutritionId = 5, Name = "Almonds", FoodGroup = "Nuts", Calories = 164, Protein = 6.0, Fat = 14.0, Carbohydrates = 6 }
-            };
+                var entries = new List<NutritionEntry>
+                {
+                    new NutritionEntry { NutritionId = 1, Name = "Apple", FoodGroup = "Fruits", Calories = 95, Protein = 0.5, Fat = 0.3, Carbohydrates = 25 },
+                    new NutritionEntry { NutritionId = 2, Name = "Chicken Breast", FoodGroup = "Meats", Calories = 165, Protein = 31, Fat = 3.6, Carbohydrates = 0 },
+                    new NutritionEntry { NutritionId = 3, Name = "Broccoli", FoodGroup = "Vegetables", Calories = 55, Protein = 4.0, Fat = 0.6, Carbohydrates = 11 },
+                    new NutritionEntry { NutritionId = 4, Name = "Rice", FoodGroup = "Grains", Calories = 206, Protein = 4.3, Fat = 0.4, Carbohydrates = 45 },
+                    new NutritionEntry { NutritionId = 5, Name = "Almonds", FoodGroup = "Nuts", Calories = 164, Protein = 6.0, Fat = 14.0, Carbohydrates = 6 }
+                };
 
-            context.AddRange(entries);
-            context.SaveChanges();
-
+                context.AddRange(entries);
+                context.SaveChanges();
+            }
         }
-
     }
 }
